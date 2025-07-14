61dc5b93e8a0e9b0044ba15783d8690baf9ceada
Vincent than <vtan558@gmail.com>
2025-07-15 00:18:50 +1000

    Create menuData.js

---
+++ menu/app/menu/menuData.js
@@ -0,0 +1,211 @@
+// File: menu/app/menu/menuData.js
+
+const menuData = [
+  // Main courses
+  {
+    id: 1,
+    name: 'Beef Wellington',
+    imageUrl: '/images/beef_wellington.jpg',
+    description1: 'Tender beef fillet wrapped in puff pastry with mushroom duxelles and prosciutto served with red wine jus.',
+    description2: 'A classic dish with a modern twist, served with seasonal vegetables.',
+    description3: 'A rich and flavorful dish that is perfect for special occasions.',
+    price: '36.00',
+    category: 'main'
+  },
+  {
+    id: 2,
+    name: 'Singapore Chilli Crab',
+    imageUrl: '/images/chilli-crab.jpg',
+    description1: 'A spicy and tangy dish featuring fresh crab cooked in a rich chilli sauce.',
+    description2: 'Served with mantou buns for dipping, this dish is a must-try.',
+    description3: 'A signature dish that showcases the bold flavors of Singaporean cuisine.',
+    price: '48.00',
+    category: 'main'
+  },
+  {
+    id: 3,
+    name: 'Aglio e Olio Pasta',
+    imageUrl: '/images/aglio-olio.jpg',
+    description1: 'Spaghetti tossed in a garlic and olive oil sauce, topped with parsley and chili flakes.',
+    description2: 'A simple yet flavorful pasta dish that is perfect for a quick meal.',
+    description3: 'A vegetarian option that is both satisfying and delicious.',
+    price: '18.00',
+    category: 'main'
+  },
+  {
+    id: 4,
+    name: 'Australian BBQ Pork Ribs',
+    imageUrl: '/images/pork-ribs.jpg',
+    description1: 'Tender pork ribs slow-cooked in a tangy BBQ sauce, served with coleslaw.',
+    description2: 'A hearty dish that is perfect for meat lovers.',
+    description3: 'Enjoy the smoky flavors of Australian BBQ with these succulent ribs.',
+    price: '36.00',
+    category: 'main'
+  },
+  {
+    id: 5,
+    name: 'Roast Duck',
+    imageUrl: '/images/roasted-duck.jpg',
+    description1: 'Crispy duck breast served with a sweet and savory plum sauce.',
+    description2: 'A traditional dish that is loved for its rich flavors and crispy skin.',
+    description3: 'Perfectly roasted and served with seasonal vegetables.',
+    price: '22.00',
+    category: 'main'
+  },
+  {
+    id: 6,
+    name: 'Wagyu A5 Steak',
+    imageUrl: '/images/wagyuA5.jpg',
+    description1: 'The finest Wagyu beef, grilled to perfection and served with a rich red wine sauce.',
+    description2: 'A luxurious dish that is sure to impress any meat lover.',
+    description3: 'Tender, juicy, and full of flavor, this steak is a true indulgence.',
+    price: '68.00',
+    category: 'main'
+  },
+  {
+    id: 7,
+    name: 'Stir fry Noodles with Seafood',
+    imageUrl: '/images/seafood-noodle.jpg',
+    description1: 'A delightful mix of fresh seafood and vegetables stir-fried with noodles in a savory sauce.',
+    description2: 'A popular dish that brings together the flavors of the sea and the crunch of fresh veggies.',
+    description3: 'Perfectly cooked noodles tossed with shrimp, squid, and seasonal vegetables.',
+    price: '28.00',
+    category: 'main'
+  },
+  {
+    id: 8,
+    name: 'Beef Burger',
+    imageUrl: '/images/beef-burger.jpg',
+    description1: 'Juicy beef patty grilled to perfection and served with fresh lettuce, tomato, and cheese.',
+    description2: 'A classic burger that is always a crowd-pleaser.',
+    description3: 'Served with a side of crispy fries and a tangy dipping sauce.',
+    price: '18.00',
+    category: 'main'
+  },
+
+  //desserts
+  {
+    id: 9,
+    name: 'Black Forest Cake',
+    imageUrl: '/images/black-forest.jpg',
+    description1: 'A rich chocolate cake with layers of cherry filling and whipped cream.',
+    description2: 'A classic German dessert that is both decadent and delicious.',
+    description3: 'Perfect for special occasions or as a sweet treat any time.',
+    price: '12.00',
+    category: 'dessert'
+  },
+  {
+    id: 10,
+    name: 'Brownies',
+    imageUrl: '/images/brownies.jpg',
+    description1: 'Rich and fudgy brownies topped with a layer of chocolate ganache.',
+    description2: 'A classic dessert that is always a favorite.',
+    description3: 'Perfect for chocolate lovers and served with a scoop of vanilla ice cream.',
+    price: '10.00',
+    category: 'dessert'
+  },
+  {
+    id: 11,
+    name: 'chocolate lava cake',
+    imageUrl: '/images/choco-lava.jpg',
+    description1: 'A rich chocolate cake with a gooey molten center, served with vanilla ice cream.',
+    description2: 'A decadent dessert that is perfect for chocolate lovers.',
+    description3: 'Indulge in the warm, gooey goodness of this classic dessert.',
+    price: '12.00',
+    category: 'dessert'
+  },
+  {
+    id: 12,
+    name: 'Coconut Cake',
+    imageUrl: '/images/coconut.jpg',
+    description1: 'A moist and fluffy coconut cake topped with creamy coconut frosting.',
+    description2: 'A tropical delight that is perfect for coconut lovers.',
+    description3: 'Light, refreshing, and full of coconut flavor, this cake is a must-try.',
+    price: '10.00',
+    category: 'dessert'
+  },
+  {
+    id: 13,
+    name: 'Lemon meringue pie',
+    imageUrl: '/images/lemon-pie.jpg',
+    description1: 'A classic dessert with a tangy lemon filling and fluffy meringue topping.',
+    description2: 'Light, refreshing, and perfect for citrus lovers.',
+    description3: 'A delightful way to end any meal on a sweet note.',
+    price: '12.00',
+    category: 'dessert'
+  },
+  {
+    id: 14,
+    name: 'Mango delight',
+    imageUrl: '/images/mango.jpg',
+    description1: 'A tropical dessert made with fresh mango puree and coconut cream.',
+    description2: 'Light, refreshing, and bursting with mango flavor, this dessert is a must-try.',
+    description3: 'Served with a side of coconut sorbet for an extra touch of indulgence.',
+    price: '10.00',
+    category: 'dessert'
+  },
+  {
+    id: 15,
+    name: 'Tiramisu',
+    imageUrl: '/images/tiramisu.jpg',
+    description1: 'A classic Italian dessert made with layers of coffee-soaked ladyfingers and mascarpone cheese.',
+    description2: 'Rich, creamy, and full of flavor, this dessert is a true indulgence.',
+    description3: 'Perfect for coffee lovers and served with a dusting of cocoa powder.',
+    price: '12.00',
+    category: 'dessert'
+  },
+  {
+    id: 16,
+    name: 'Banana Split',
+    imageUrl: '/images/banana.jpg',
+    description1: 'A classic dessert made with sliced bananas, scoops of ice cream, and topped with chocolate sauce.',
+    description2: 'Perfect for sharing or enjoying on your own.',
+    description3: 'Served with whipped cream and a cherry on top.',
+    price: '10.00',
+    category: 'dessert'
+  },
+  
+  // Drinks
+  {
+    id: 17,
+    name: 'Amalfi',
+    imageUrl: '/images/amalfi.jpg',
+    description1: 'A refreshing cocktail made with gin, lemon juice, and tonic water.',
+    description2: 'Perfect for a sunny day, this drink is light and invigorating.',
+    description3: 'Served with a slice of lemon and a sprig of mint for added freshness.',
+    price: '12.00',
+    category: 'drink'
+  },
+  {
+    id: 18,
+    name: 'Martini',
+    imageUrl: '/images/martini.jpg',
+    description1: 'A classic cocktail made with gin and vermouth, garnished with an olive.',
+    description2: 'Sophisticated and timeless, this drink is perfect for any occasion.',
+    description3: 'Served chilled and stirred to perfection, it’s a favorite among cocktail enthusiasts.',
+    price: '14.00',
+    category: 'drink'
+  },
+  {
+    id: 19,
+    name: 'Aperol Spritz',
+    imageUrl: '/images/aperol.jpg',
+    description1: 'A refreshing cocktail made with Aperol, Prosecco, and soda water.',
+    description2: 'Light, bubbly, and perfect for warm weather, this drink is a crowd-pleaser.',
+    description3: 'Garnished with an orange slice for a touch of citrusy brightness.',
+    price: '12.00',
+    category: 'drink'
+  },
+  {
+    id: 20,
+    name: 'Mojito',
+    imageUrl: '/images/mojito.jpg',
+    description1: 'A refreshing cocktail made with rum, mint, lime, and soda water.',
+    description2: 'Light, minty, and perfect for warm weather, this drink is a favorite.',
+    description3: 'Garnished with fresh mint and a lime wedge for added flavor.',
+    price: '12.00',
+    category: 'drink'
+  }
+];
+
+export default menuData;
